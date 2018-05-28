08de69b3be5d6d2ecf0201e86d2bc49a3cd2fd02
Kyle DeFrancia <kyle.def@zoho.com>
2018-05-28 08:08:01 -0700

    create a buffer to draw on

--- main.c
+++ main.c
@@ -1,5 +1,24 @@
+#include <stdlib.h>
 #include <stdio.h>
 #include <GLFW/glfw3.h>
+
+typedef struct {
+	size_t width;
+	size_t height;
+	uint32_t *data;
+} Buffer;
+
+uint32_t rgb_to_uint32(uint8_t r, uint8_t g, uint8_t b) {
+	//use rightmost 8bits as alpha, ignored for now
+	return (r << 24) | (g << 16) | (b << 8) | 255;
+}
+
+void buffer_clear(Buffer *buffer, uint32_t color) {
+	size_t i;
+	for (i = 0; i < (buffer->width * buffer->height); i++) {
+		buffer->data[i] = color;
+	}
+}
 
 void error_cb(int errno, const char *description) {
 	fprintf(stderr, "GLFW Error: %s\n", description);
@@ -13,12 +32,20 @@ void key_cb(GLFWwindow *window, int key, int scancode, int action, int mods)
 
 int main(int argc, char *argv[]) {
 	GLFWwindow *window;
+	Buffer buffer;
+
+	buffer.width = 640;
+	buffer.height = 480;
+	buffer.data = malloc(sizeof (uint32_t) * (buffer.width * buffer.height));
+
+	uint32_t clear_color = rgb_to_uint32(0, 255, 0);
+	buffer_clear(&buffer, clear_color);
 
 	glfwSetErrorCallback(error_cb);
 	if (!glfwInit())
 		return -1;
 
-	window = glfwCreateWindow(640, 480, "Space Invaders!", NULL, NULL);
+	window = glfwCreateWindow(buffer.width, buffer.height, "Space Invaders!", NULL, NULL);
 	if (!window) {
 		glfwTerminate();
 		return -2;
