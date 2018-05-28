6af58afb0271c8238baa74b2a96584c2e543efa4
Kyle DeFrancia <kyle.def@zoho.com>
2018-05-28 06:56:34 -0700

    create a window with GLFW 3

---
+++ Makefile
@@ -0,0 +1,2 @@
+all: main.c
+	gcc -o spaceinvaders main.c -lglfw -lGL
---
+++ main.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <GLFW/glfw3.h>
+
+void error_cb(int errno, const char *description) {
+	fprintf(stderr, "GLFW Error: %s\n", description);
+}
+
+int main(int argc, char *argv[]) {
+	GLFWwindow *window;
+
+	glfwSetErrorCallback(error_cb);
+	if (!glfwInit())
+		return -1;
+
+	window = glfwCreateWindow(640, 480, "Space Invaders!", NULL, NULL);
+	if (!window) {
+		glfwTerminate();
+		return -2;
+	}
+
+	glfwMakeContextCurrent(window);
+
+	while (!glfwWindowShouldClose(window)) {
+		int width;
+		int height;
+		glfwGetFramebufferSize(window, &width, &height);
+		glViewport(0, 0, width, height);
+	}
+
+	glfwDestroyWindow(window);
+	glfwTerminate();
+	return 0;
+}
